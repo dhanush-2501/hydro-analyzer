51e0534b986368506757511dad1f82531c1b8214
dhanush <ibg2501@gmail.com>
2023-03-05 17:34:11 +0530

    modified inheritance and ui setup

--- HyAn/widgets/main_window.py
+++ HyAn/widgets/main_window.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 # This Python file uses the following encoding: utf-8
 
-import sys
-
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QMainWindow
 
 
 from HyAn.ui.ui_main_window import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        QMainWindow.__init__(self, parent)
+        self.setupUi(self)
 
 
 
