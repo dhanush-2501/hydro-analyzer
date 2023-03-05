c3596eda3b42f463522e0b4f003a8a94027b185a
dhanush <ibg2501@gmail.com>
2023-03-05 17:11:11 +0530

    changed class name

--- HyAn/widgets/pumping_data.py
+++ HyAn/widgets/pumping_data.py
@@ -2,7 +2,7 @@ from PySide6.QtWidgets import QWidget
 
 from HyAn.ui.ui_pumping_data import Ui_PumpingData
 
-class PumpingTest(QWidget, Ui_PumpingData):
+class PumpingData(QWidget, Ui_PumpingData):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setupUi(self)
