666e528d7f8ac21279107c72d78c098575f09501
dhanush <ibg2501@gmail.com>
2023-03-10 19:12:06 +0530

    created widgets for plot

---
+++ HyAn/widgets/analysis_curve.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+class AnalysisCurve(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
---
+++ HyAn/widgets/drawdown_curve.py
@@ -0,0 +1,7 @@
+from PySide6.QtWidgets import QWidget
+
+
+class DrawdownCurve(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
