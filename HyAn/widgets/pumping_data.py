203ca6f4f8c5636367351b9ecc8935b5e384132a
dhanush <ibg2501@gmail.com>
2023-03-14 17:27:11 +0530

    added comments

--- HyAn/widgets/pumping_data.py
+++ HyAn/widgets/pumping_data.py
@@ -12,12 +12,14 @@ class PumpingData(QWidget, Ui_PumpingData):
         QWidget.__init__(self, parent)
         self.setupUi(self)
 
-        self.labels = ["Time [min]", "Drawdown [m]"]
+        self.labels = ["Time [min]", "Drawdown [m]"] # labels for table
 
+        # create table
         self.table_drawdown.setRowCount(20)
         self.table_drawdown.setColumnCount(2)
         self.table_drawdown.setHorizontalHeaderLabels(self.labels)
 
+        # get data from table
         self.btn_plt_data.clicked.connect(self.get_data)
 
     def get_data(self):
@@ -35,9 +37,9 @@ class PumpingData(QWidget, Ui_PumpingData):
         #              [38, 4.7], [47, 5.1], [50, 5.3], [60, 5.7], [70, 6.1], [80, 6.3], [90, 6.7],
         #              [100, 7.0], [130, 7.5], [160, 8.3], [200, 8.5], [260, 9.2], [320, 9.7], [380, 10.2], [500, 10.9]]
 
-        self.wid_plt_graph.plot_data(self.data)
+        self.wid_plt_graph.plot_data(self.data) # plot data
         print("get data : ", self.data)
-        self.data_changed.emit([self.data, float(self.l_edit_constant.text())])
+        self.data_changed.emit([self.data, float(self.l_edit_constant.text())]) # send data
         return self.data
 
     def get_report(self):
