00b817f8355443eb9b12db91c14fe05007f66077
dhanush <ibg2501@gmail.com>
2023-03-14 15:17:01 +0530

    code rfactored

--- HyAn/hyan.py
+++ HyAn/hyan.py
@@ -2,9 +2,9 @@ import sys
 from PySide6.QtWidgets import QApplication
 from HyAn.widgets.main_window import MainWindow
 
+
 def main():
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
     sys.exit(app.exec())
-
